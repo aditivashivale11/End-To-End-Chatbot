529c58ef756a4e4012e6d8fcf2919f14fbe69bc2
aditivashivale11 <aditivashivale@gmail.com>
2025-01-03 00:26:11 +0530

    new commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='End To End Chatbot',
+    version='0.0.1',
+    author='aditi',
+    author_email='aditivashivale@gmail.com',
+    packages=find_packages()
+    install_requires=['pandas', 'numpy', 'streamlit', 'nltk', 'ramdom']
+    
+)
